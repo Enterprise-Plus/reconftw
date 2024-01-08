96143464413625d84a57c00787459776e8be267f
Ciprian Ursulean <ciprian.ursulean@visma.com>
2024-01-08 11:36:21 +0200

    Commented Telegram and Discord notifications

--- reconftw.sh
+++ reconftw.sh
@@ -2037,17 +2037,17 @@ function sendToNotify {
 			transfer "${1}" | notify
 			return 0
 		fi
-		if grep -q '^ telegram\|^telegram\|^    telegram' $NOTIFY_CONFIG ; then
-			notification "Sending ${domain} data over Telegram" info
-			telegram_chat_id=$(cat ${NOTIFY_CONFIG} | grep '^    telegram_chat_id\|^telegram_chat_id\|^    telegram_chat_id' | xargs | cut -d' ' -f2)
-			telegram_key=$(cat ${NOTIFY_CONFIG} | grep '^    telegram_api_key\|^telegram_api_key\|^    telegram_apikey' | xargs | cut -d' ' -f2 )
-			curl -F document=@${1} "https://api.telegram.org/bot${telegram_key}/sendDocument?chat_id=${telegram_chat_id}" 2>>"$LOGFILE" >/dev/null
-		fi
-		if grep -q '^ discord\|^discord\|^    discord' $NOTIFY_CONFIG ; then
-			notification "Sending ${domain} data over Discord" info
-			discord_url=$(cat ${NOTIFY_CONFIG} | grep '^ discord_webhook_url\|^discord_webhook_url\|^    discord_webhook_url' | xargs | cut -d' ' -f2)
-			curl -v -i -H "Accept: application/json" -H "Content-Type: multipart/form-data" -X POST -F file1=@${1} $discord_url 2>>"$LOGFILE" >/dev/null
-		fi
+		# if grep -q '^ telegram\|^telegram\|^    telegram' $NOTIFY_CONFIG ; then
+		# 	notification "Sending ${domain} data over Telegram" info
+		# 	telegram_chat_id=$(cat ${NOTIFY_CONFIG} | grep '^    telegram_chat_id\|^telegram_chat_id\|^    telegram_chat_id' | xargs | cut -d' ' -f2)
+		# 	telegram_key=$(cat ${NOTIFY_CONFIG} | grep '^    telegram_api_key\|^telegram_api_key\|^    telegram_apikey' | xargs | cut -d' ' -f2 )
+		# 	curl -F document=@${1} "https://api.telegram.org/bot${telegram_key}/sendDocument?chat_id=${telegram_chat_id}" 2>>"$LOGFILE" >/dev/null
+		# fi
+		# if grep -q '^ discord\|^discord\|^    discord' $NOTIFY_CONFIG ; then
+		# 	notification "Sending ${domain} data over Discord" info
+		# 	discord_url=$(cat ${NOTIFY_CONFIG} | grep '^ discord_webhook_url\|^discord_webhook_url\|^    discord_webhook_url' | xargs | cut -d' ' -f2)
+		# 	curl -v -i -H "Accept: application/json" -H "Content-Type: multipart/form-data" -X POST -F file1=@${1} $discord_url 2>>"$LOGFILE" >/dev/null
+		# fi
 		if [[ -n "$slack_channel" ]] && [[ -n "$slack_auth" ]]; then
 			notification "Sending ${domain} data over Slack" info
 			curl -F file=@${1} -F "initial_comment=reconftw zip file" -F channels=${slack_channel} -H "Authorization: Bearer ${slack_auth}" https://slack.com/api/files.upload 2>>"$LOGFILE" >/dev/null
